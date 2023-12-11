6e4454bd109d0dca8da1e11d99a0276f66db27b0
Ariy83 <143614766+Ariy83@users.noreply.github.com>
2023-12-11 17:00:34 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { Buttons } from "./Buttons/Buttons"
 import { Statistics } from "./Statistics/Statistics"
 
@@ -18,58 +18,104 @@ const Notification = ({message}) => {
   )
 }
 
-export class App extends React.Component {
-state = {
-  good: 0,
-  neutral: 0,
-  bad: 0
-  }
+export const App = () => {
+  const [feedBack, setFeedBack] = useState({ good: 0, neutral: 0, bad: 0 })
   
-  getStateKeys = () => {
-    return Object.keys(this.state)
+  const getStateKeys = () => {
+    return Object.keys(feedBack)
   }
-  getStateValues = () => {
-    return Object.values(this.state)
-  }
-
-  handleClickBtn = (item) => {
-    this.setState((prevState) => ({ [item]: prevState[item] + 1 }));
-    
+  const getStateValues = () => {
+    return Object.values(feedBack)
   }
 
-  countTotalFeedback = () => {
-    return this.getStateValues().reduce((total,item)=>(total+=item))
+  const handleClickBtn = (item) => {
+    setFeedBack((prevState) => ({...prevState, [item]: prevState[item] + 1 }));
+  }
+  const countTotalFeedback = () => {
+    return getStateValues().reduce((total,item)=>(total+=item))
+  }
+  const countPositiveFeedbackPercentage = () => {
+    if (feedBack.good===0){return}
+    return feedBack.good*100/countTotalFeedback()
   }
 
-  countPositiveFeedbackPercentage = () => {
-    if (this.state.good===0){return}
-    return this.state.good*100/this.countTotalFeedback()
-  }
-
-  render() {
-   return <div
+  return <div
       style={{
         height: '100vh',
        display: 'flex',
         flexDirection: 'column',
-        // justifyContent: 'center',
         alignItems: 'center',
         fontSize: 30,
         color: '#010101'
       }}
    >
     <Section title="Please leave feedback">
-      <Buttons keysArr={this.getStateKeys()} handleClickBtn={this.handleClickBtn} />
+      <Buttons keysArr={getStateKeys()} handleClickBtn={handleClickBtn} />
      </Section>
      
      <Section title="Statistics">
-       {!this.countTotalFeedback() ?
+       {!countTotalFeedback() ?
          (<Notification message="There is no feedback" />) :
-         (<><Statistics keysArr={this.getStateKeys()} valuesArr={this.getStateValues()} />
-         <p>Total: { this.countTotalFeedback()}</p>
-         <p>Positive feedback: { Math.round(this.countPositiveFeedbackPercentage())}%</p></>)}
+         (<><Statistics keysArr={getStateKeys()} valuesArr={getStateValues()} />
+         <p>Total: { countTotalFeedback()}</p>
+         <p>Positive feedback: { Math.round(countPositiveFeedbackPercentage())}%</p></>)}
      </Section>
      
     </div>
-  };
-};
+}
+
+// export class App extends React.Component {
+// state = {
+//   good: 0,
+//   neutral: 0,
+//   bad: 0
+//   }
+  
+  // getStateKeys = () => {
+  //   return Object.keys(this.state)
+  // }
+  // getStateValues = () => {
+  //   return Object.values(this.state)
+  // }
+
+  // handleClickBtn = (item) => {
+  //   this.setState((prevState) => ({ [item]: prevState[item] + 1 }));
+    
+  // }
+
+  // countTotalFeedback = () => {
+  //   return this.getStateValues().reduce((total,item)=>(total+=item))
+  // }
+
+  // countPositiveFeedbackPercentage = () => {
+  //   if (this.state.good===0){return}
+  //   return this.state.good*100/this.countTotalFeedback()
+  // }
+
+//   render() {
+//    return <div
+//       style={{
+//         height: '100vh',
+//        display: 'flex',
+//         flexDirection: 'column',
+//         // justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 30,
+//         color: '#010101'
+//       }}
+//    >
+//     <Section title="Please leave feedback">
+//       <Buttons keysArr={this.getStateKeys()} handleClickBtn={this.handleClickBtn} />
+//      </Section>
+     
+//      <Section title="Statistics">
+//        {!this.countTotalFeedback() ?
+//          (<Notification message="There is no feedback" />) :
+//          (<><Statistics keysArr={this.getStateKeys()} valuesArr={this.getStateValues()} />
+//          <p>Total: { this.countTotalFeedback()}</p>
+//          <p>Positive feedback: { Math.round(this.countPositiveFeedbackPercentage())}%</p></>)}
+//      </Section>
+     
+//     </div>
+//   };
+// };
